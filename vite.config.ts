4f28ef0dbdfa9cc490f1a3002d197a590a74fb6e
harish khileri <harishkhilery98@gmail.com>
2025-06-26 14:31:04 +0530

    modified vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // ✅ Important for Vercel
+  base: '/quick-react-website-builder/',
   server: {
     host: "::",
     port: 8080,
